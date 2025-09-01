8ad31a68c2eab12f1579e6aa21193e01c346cad5
JaahanviSingh19 <130581106+sjaahanvi@users.noreply.github.com>
2025-09-01 09:50:47 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
+const serviceRoute = require("./router/service-router");
+const adminRoute = require("./router/admin-router");
+const connectDb = require("./utils/db");
+const errorMiddleware = require("./middlewares/error-middleware");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "https://thapatechnical.site",
+      "https://www.thapatechnical.site",
+    ];
+    const isAllowed = allowedOrigins.includes(origin);
+    callback(null, isAllowed ? origin : false);
+  },
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+app.use("/api/data", serviceRoute);
+
+app.use("/api/admin", adminRoute);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 5001;
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
+});
